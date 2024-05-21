1a1931ad0bd079b185d1f025ac4721cdb502605f
KABAN9KA <93037269+KABAN9KA@users.noreply.github.com>
2024-05-21 18:42:03 +0300

    Delete vhandlog directory

--- vhandlog/db.php
+++
@@ -1,3 +0,0 @@
-<?php
-    session_start();
-    $pdo = new PDO('mysql:hosts=localhost;dbname=sait;', 'root', 'root');
--- vhandlog/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-    require "db.php";
-    unset($_SESSION['login']);
-    header("Location: /");
